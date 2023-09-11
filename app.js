c6c1852dd92a6440228707c766dd1fdbf33ceb21
unknown <vlikunama@gmail.com>
2023-09-11 16:19:07 +0300

    initial commit

--- app.js
+++ app.js
@@ -5,17 +5,18 @@ const http = require('http');
 const PORT = 4000;
 
 let currentDate = new Date()
-/*let utc_time = currentDate.toISOString().slice(0,-5)+'Z';/*let utc_time = currentDate.toISOString().slice(0,-5)+'Z'; */
+let utc_time = currentDate.toISOString(0,5)+'Z';/*let utc_time = currentDate.toISOString().slice(0,-5)+'Z'; */
 const currentDateNum = currentDate.getDay();
 const daysInWeek = ['Sunday','Monday' ,'Tuesday' ,'Wensday','Thursday','Friday','Saturday'];
 const currentDayToday = daysInWeek[currentDateNum];
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'application/json'});
-  res.end(JSON.stringify({
+  res.end(
+    JSON.stringify({
  "slack_name": "Vee",
   "current_day": `${currentDayToday}`,
-  "utc_time": `${utc_time = currentDate.toISOString(0,-5)}`,
+  "utc_time": `${utc_time}`,
   "track": "backend",
   "github_file_url": "https://github.com/veemish/taskone_simple_api/blob/main/app.js",
   "github_repo_url": "https://github.com/veemish/taskone_simple_api",
