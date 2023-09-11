5993ae9f943f00d167258e98702944e615707fe8
unknown <vlikunama@gmail.com>
2023-09-11 13:21:41 +0300

    testing

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
 
+const http = require('http');
+const PORT = 4000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('hello world');
+});
+
+server.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`)
+})
+
 
 
 
