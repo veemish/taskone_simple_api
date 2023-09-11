26df7c3cdace95766714cccfd923131124524158
unknown <vlikunama@gmail.com>
2023-09-11 14:39:50 +0300

    added time in  UTC and date

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ const server = http.createServer((req, res) => {
   "track": "backend",
   "github_file_url": "https://github.com/veemish/taskone_simple_api/blob/main/app.js",
   "github_repo_url": "https://github.com/veemish/taskone_simple_api",
-  "status_code": 200} ));
+  "status_code": 200},
+   ));
 });
 
 server.listen(PORT, () => {
@@ -26,11 +27,36 @@ server.listen(PORT, () => {
   res.end('some data')*/
 
   app.get('/api?slack_name=Vee&&track=backend', (req, res) => {
-    // handle the request
+    // handle the reques
     res.send('application/json')
   })
 
 
+
+app.get("/api?slack_name=Vee&&track=backend",(req, res) => {
+   let slack_name = req.query.slack_name
+    let track = req.query.track
+    /*Date time*/
+   let currentDate = new Date()
+    let utc_time = currentDate.toISOString().slice(0, -5) +'Z';
+    const currentDateNum = currentDate.getDay();
+    const daysInWeek = ['Monday' ,'Tuesday' ,'Wensday','Thursday','Friday','Saturday','Sunday'];
+    const currentDayToday = daysInWeek[currentDateNum];
+
+
+    const details = {
+        "slack_name":`${slack_name}`,
+        "current_day":`${currentDayToday}`,
+        'utc_time':`${utc_time}`,
+        "track":`${track}`,
+        "github_file_url": "https://github.com/veemish/taskone_simple_api/blob/main/app.js",
+        "github_repo_url": "https://github.com/veemish/taskone_simple_app",
+        "status_code": 200,
+    }
+
+    res.status(200).json(details)
+    
+});
 
 
 
