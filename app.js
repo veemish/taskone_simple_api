a112d4eeaf985345584aeb41f9072f9835676b95
unknown <vlikunama@gmail.com>
2023-09-11 12:41:24 +0300

    modied GET request

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
-/*const port = 5000;
-app.listen(port,() => console.log(`App is listening on port ${port}!`));*/
+app.use(express.json());
+const port = process.env.PORT || 5000;
+
+
+app.listen(port,() => console.log(`App is listening on port ${port}!`));
 
   /*  const headSetter = (req, res, next) => {
     res.setHeader('Content-Type', 'application/json')
@@ -11,7 +14,7 @@ app.listen(port,() => console.log(`App is listening on port ${port}!`));*/
 
     }
 app.use(headSetter)*/
-app.get('/api?slack_name=Vee&&track=backend',(req, res) => {
+/*app.get('/api?slack_name=Vee&&track=backend',(req, res) => {
     res.send([
         {
             "slack_name": "Vee",
@@ -25,19 +28,22 @@ app.get('/api?slack_name=Vee&&track=backend',(req, res) => {
     ]);
 });
 
+app.use('/', express.static('/build'))
+
+
 app.get("/:universalURL", (req, res) => {
     res.send("404 URL NOT FOUND");
-});
+});*/
 
-app.listen(5000, () => {
+/*app.listen(5000, () => {
     console.log("listening on http://localhost:5000");
-})
-/*
-app.get("/api",(req, res) => {
+})*/
+
+app.get("/api?slack_name=Vee&&track=backend",(req, res) => {
    let slack_name = req.query.slack_name
     let track = req.query.track
     /*Date time*/
-   /* let currentDate = new Date()
+   let currentDate = new Date()
     let utc_time = currentDate.toISOString().slice(0, -5) +'Z';
     const currentDateNum = currentDate.getDay();
     const daysInWeek = ['Monday' ,'Tuesday' ,'Wensday','Thursday','Friday','Saturday','Sunday'];
@@ -50,7 +56,7 @@ app.get("/api",(req, res) => {
         'utc_time':`${utc_time}`,
         "track":`${track}`,
         "github_file_url": "https://github.com/veemish/taskone_simple_api/blob/main/app.js",
-        "github_repo_url": "https://github.com/veemish/taskone_simple_api",
+        "github_repo_url": "https://github.com/veemish/taskone_simple_app",
         "status_code": 200,
     }
 
@@ -62,7 +68,7 @@ app.get("/api",(req, res) => {
 
 app.get('*', (req, res) => {
     res.status(404).json({"error":"The page your looking for is not here"})
-})*/
+})
 
 
 
