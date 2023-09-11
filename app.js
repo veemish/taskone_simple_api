9e17a03f7325f44e0a8f6b8b859bf6bbc1cd0e7b
unknown <vlikunama@gmail.com>
2023-09-11 18:23:07 +0300

    initial commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = 4000;
 
 
 let currentDate = new Date() 
-let utc_time = currentDate.toISOString().slice(0,19)+"Z";/*let utc_time = currentDate.toISOString().slice(0,-5)+'Z'; */
+let utc_time = currentDate.toISOString().slice(0,2)+"Z";/*let utc_time = currentDate.toISOString().slice(0,-5)+'Z'; */
 const currentDateNum = currentDate.getDay();
 const daysInWeek = ['Sunday','Monday' ,'Tuesday' ,'Wensday','Thursday','Friday','Saturday'];
 const currentDayToday = daysInWeek[currentDateNum];
