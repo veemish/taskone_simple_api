662c2ed2ee73dc135721e36a3244f5343eabd1fe
unknown <vlikunama@gmail.com>
2023-09-11 22:30:26 +0300

    utc time added

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ const server = http.createServer((req, res) => {
     JSON.stringify({
  "slack_name": "Vee",
   "current_day": `${currentDayToday}`,
-  "utc_time": utc_time=currentDate.toISOString().slice(0,-5)+"Z",
+  "utc_time": `${utc_time}`,
   "track": "backend",
   "github_file_url": "https://github.com/veemish/taskone_simple_api/blob/main/app.js",
   "github_repo_url": "https://github.com/veemish/taskone_simple_api",
