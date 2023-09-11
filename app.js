2527bec12b2d53b4a2aed4266099b7ffa2c73ee9
unknown <vlikunama@gmail.com>
2023-09-11 16:04:52 +0300

    Trying a different approach

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const http = require('http');
 const PORT = 4000;
 
 let currentDate = new Date()
-let utc_time = currentDate.toISOString(0,-5)+'Z';/*let utc_time = currentDate.toISOString().slice(0,-5)+'Z'; */
+/*let utc_time = currentDate.toISOString().slice(0,-5)+'Z';/*let utc_time = currentDate.toISOString().slice(0,-5)+'Z'; */
 const currentDateNum = currentDate.getDay();
 const daysInWeek = ['Sunday','Monday' ,'Tuesday' ,'Wensday','Thursday','Friday','Saturday'];
 const currentDayToday = daysInWeek[currentDateNum];
@@ -15,7 +15,7 @@ const server = http.createServer((req, res) => {
   res.end(JSON.stringify({
  "slack_name": "Vee",
   "current_day": `${currentDayToday}`,
-  "utc_time": `${utc_time}`,
+  "utc_time": `${utc_time = currentDate.toISOString().slice(0,-5)+'Z'}`,
   "track": "backend",
   "github_file_url": "https://github.com/veemish/taskone_simple_api/blob/main/app.js",
   "github_repo_url": "https://github.com/veemish/taskone_simple_api",
